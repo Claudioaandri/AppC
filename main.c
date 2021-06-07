990026ef376dfc968ad56ff923475746c123d854
Claudioaadri <claudioaandrian10@gmail.com>
2021-06-07 20:39:51 +0300

    init

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     /*
+     Programme qui demande a l'utisateur un nombre qu'il affiche par la suite
+
+     Declaration des variables
+
+    */
+
+    var i : integer; //pascal
+    var ch : string[40];
+  //  int i;
+
+
+     Writeln("Enter number");
+//    printf("Entrer un chiffre : ");
+
+    //Affichage
+    readln(i);
+    //scanf("%d", &i);
+
+    //printf("Le chiffre que vous avez entres est : %d ", &i);
+
+    Writeln("number : " i);
+
+}
